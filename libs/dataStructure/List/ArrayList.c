a1da466ac0fe0b25eb8337416122d17603c3bad7
UseR <147231882+BogdanPryadko4853@users.noreply.github.com>
2024-04-23 19:55:20 +0300

    start ArrayList

--- libs/dataStructure/List/ArrayList.c
+++ libs/dataStructure/List/ArrayList.c
@@ -1,3 +1,20 @@
 //
 // Created by Bogdan on 22.04.2024.
 //
+#include "ArrayList.h"
+
+void addNewElement(ArrayList **arrayListPtr, int element) {
+    ArrayList *arrayList = *arrayListPtr;
+    // Увеличиваем размер выделенной памяти
+    arrayList = realloc(arrayList, (arrayList->num + 1) * sizeof(ArrayList));
+    if (arrayList == NULL) {
+        // Обработка ошибки выделения памяти
+        fprintf(stderr, "Ошибка выделения памяти\n");
+        exit(1);
+    }
+    // Добавляем новый элемент
+    arrayList[arrayList->num].val = element;
+    arrayList->num++;
+    // Обновляем указатель
+    *arrayListPtr = arrayList;
+}
--- libs/dataStructure/List/ArrayList.h
+++ libs/dataStructure/List/ArrayList.h
@@ -2,7 +2,29 @@
 // Created by Bogdan on 22.04.2024.
 //
 
+
 #ifndef RELISSTRUCTURE_ARRAYLIST_H
 #define RELISSTRUCTURE_ARRAYLIST_H
+
+#include <stdbool.h>
+#include <malloc.h>
+#include <stdio.h>
+
+typedef struct arrayList {
+    int val;
+    int num; // Количество элементов
+} ArrayList;
+
+//добавление нового элемента
+void addNewElement(ArrayList **arrayListPtr, int element) ;
+
+//удаление элемента по индексу
+void deleteElementByIndex(ArrayList **arrayList,int index);
+
+//получение элемента по индексу
+int getElementByIndex(ArrayList **arrayList,int index);
+
+//проверка содержится ли элемент в листе
+bool contains(ArrayList **arrayList,int element);
 
 #endif //RELISSTRUCTURE_ARRAYLIST_H
